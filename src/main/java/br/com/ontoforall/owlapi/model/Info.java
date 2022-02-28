928fcec7cc24b25862d02ade3438e683b2128ad9
Eduardo Felipe <erfelipe@yahoo.com.br>
2022-02-28 14:45:20 -0300

    Ajustes para o merge com master

--- src/main/java/br/com/ontoforall/owlapi/model/Info.java
+++ src/main/java/br/com/ontoforall/owlapi/model/Info.java
@@ -10,7 +10,7 @@ public class Info {
 						"POST - http://200.17.70.211:13951/owlapi/webapi/ontology/valid  - retorna boolean" + System.lineSeparator() +
 						"POST - http://200.17.70.211:13951/owlapi/webapi/ontology/format - retorna string " + System.lineSeparator() +
 						"" + System.lineSeparator() +
-						"Modelo de dados para os metodos POST acima" 										+ System.lineSeparator() + 
+						"Modelo de dados antigo para os metodos POST acima" 										+ System.lineSeparator() + 
 						"	  {\n"
 						+ "	    \"id\": \"https://onto4alleditor.com/pt/idDoProjeto\",\n"
 						+ "	    \"outformat\": \"OWL\",\n"
